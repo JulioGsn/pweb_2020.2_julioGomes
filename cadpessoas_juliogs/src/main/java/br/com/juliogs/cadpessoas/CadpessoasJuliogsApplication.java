59b2cd25dfab0ce5b7efeb3c5d869a63d18e7d85
JulioGsn <julio_g15@outlook.com>
2021-04-07 00:41:24 -0300

    Iniciando CRUD da Unidade 1

---
+++ cadpessoas_juliogs/src/main/java/br/com/juliogs/cadpessoas/CadpessoasJuliogsApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliogs.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasJuliogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasJuliogsApplication.class, args);
+	}
+
+}
---
+++ cadpessoas_juliogs/src/test/java/br/com/juliogs/cadpessoas/CadpessoasJuliogsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliogs.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasJuliogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
