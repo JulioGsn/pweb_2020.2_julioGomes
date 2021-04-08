a7ced5557ca3f7323eeb0ab76a958c48caeb5dc5
JulioGsn <julio_g15@outlook.com>
2021-04-08 00:43:51 -0300

    Listar pessoas

--- cadpessoas_juliogs/src/main/java/br/com/juliogs/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_juliogs/src/main/java/br/com/juliogs/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,33 @@
 package br.com.juliogs.cadpessoas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+import br.com.juliogs.cadpessoas.model.Pessoa;
+import br.com.juliogs.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepo;
+
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
+
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
+	}
 }
