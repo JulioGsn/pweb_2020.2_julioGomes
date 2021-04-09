57db3a0c1199f5bd3bde92111820ede14aad32bb
JulioGsn <julio_g15@outlook.com>
2021-04-08 23:08:29 -0300

    Remover pessoas

--- cadpessoas_juliogs/src/main/java/br/com/juliogs/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_juliogs/src/main/java/br/com/juliogs/cadpessoas/controllers/PessoaController.java
@@ -3,6 +3,7 @@ package br.com.juliogs.cadpessoas.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,5 +30,13 @@ public class PessoaController {
 		ModelAndView mav = new ModelAndView("listarPessoas");
 		mav.addObject("pessoas", lista);
 		return mav;
+	}
+
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerPessoa(@PathVariable long id) {
+		Pessoa aRemover = pessoaRepo.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id)); 
+		pessoaRepo.delete(aRemover);
+		return new ModelAndView("redirect:/listarPessoas");
 	}
 }
