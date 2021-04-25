2781d0e9e83d078db0331907d6b67151077a005b
JulioGsn <julio_g15@outlook.com>
2021-04-25 15:17:01 -0300

    CRUD Clientes

--- AgroPopShop/src/main/java/br/com/juliogs/agropopshop/controllers/ClienteController.java
+++ AgroPopShop/src/main/java/br/com/juliogs/agropopshop/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,16 +29,40 @@ public class ClienteController {
     }
 
     @GetMapping("/clientes/adicionar")
-    public ModelAndView adicionarPessoa() {
+    public ModelAndView formCadastro() {
         ModelAndView mav = new ModelAndView("clientes/adicionar");
         mav.addObject(new Cliente());
         return mav;
     }
 
     @PostMapping("/clientes/adicionar")
-    public String adicionarPessoa(Cliente p) {
+    public String adicionar(Cliente p) {
         this.clienteRepo.save(p);
         return "redirect:/clientes";
+    }
+
+    @GetMapping("/clientes/editar/{id}")
+    public ModelAndView formEditar(@PathVariable("id") long id) {
+        Cliente cliente = this.clienteRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+
+        ModelAndView modelAndView = new ModelAndView("clientes/editar");
+        modelAndView.addObject(cliente);
+        return modelAndView;
+    }
+
+    @PostMapping("/clientes/editar/{id}")
+    public ModelAndView atualizar(@PathVariable("id") long id, Cliente cliente) {
+        this.clienteRepo.save(cliente);
+        return new ModelAndView("redirect:/clientes");
+    }
+
+    @GetMapping("/clientes/remover/{id}")
+    public ModelAndView remover(@PathVariable long id) {
+        Cliente aRemover = this.clienteRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+        this.clienteRepo.delete(aRemover);
+        return new ModelAndView("redirect:/clientes");
     }
 
 }
