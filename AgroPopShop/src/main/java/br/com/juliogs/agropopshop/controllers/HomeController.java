013d145172a1231cf71a981b58e8e1a0ce0b8fc4
JulioGsn <julio_g15@outlook.com>
2021-05-10 22:32:01 -0300

    home products list

--- AgroPopShop/src/main/java/br/com/juliogs/agropopshop/controllers/HomeController.java
+++ AgroPopShop/src/main/java/br/com/juliogs/agropopshop/controllers/HomeController.java
@@ -1,15 +1,28 @@
 package br.com.juliogs.agropopshop.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.juliogs.agropopshop.model.Produto;
+import br.com.juliogs.agropopshop.repositories.ProdutoRepository;
 
 @Controller
 @RequestMapping
 public class HomeController {
+    @Autowired
+    ProdutoRepository produtoRepo;
+
     @GetMapping("/")
-    public String index() {
-        return "home";
+    public ModelAndView index() {
+        List<Produto> lista = produtoRepo.findAll();
+        ModelAndView mav = new ModelAndView("home");
+        mav.addObject("produtos", lista);
+        return mav;
     }
 
 }
